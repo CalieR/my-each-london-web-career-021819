6082fcf523638461460c6b6a1535c39812cfb095
Calie Rushton <calielrushton@gmail.com>
2019-01-03 11:01:50 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -4,6 +4,10 @@ def my_each (array)
     yield array[i]
     i = i + 1
   end
-
   array
 end
+
+# acts like #each because it yields each element in turn
+# no block in this case so nothing happens to each element
+# while loop to keep count of where we are in the collection
+# explicit return of the original collection after the loop has finished
